e21a9933ba41b25935559fcfd61cb6775ddc75d4
Connor Turland <connor@madhattertech.ca>
2012-09-22 23:15:31 -0400

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,6 @@ has_many :items
 
   acts_as_authentic do |configuration| 
     configuration.session_class = Session
-    configuration.require_password_confirmation = false
-    
-    configuration.merge_validates_format_of_email_field_options unless: Proc.new { |user| user.email.blank? and user.authed? }
-    configuration.merge_validates_length_of_email_field_options unless: Proc.new { |user| user.email.blank? and user.authed? }
-    #configuration.merge_validates_length_of_password_field_options unless: Proc.new { |user| user.password.blank? and user.authed? }
   end
   
   validates :password, :presence => true,
