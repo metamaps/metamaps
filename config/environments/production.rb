a5fd4cb835b9a0b556d85c7ac6c810040ec7bf72
Connor Turland <connor@madhattertech.ca>
2012-12-14 02:59:37 -0500

    caching changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ ISSAD::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
@@ -12,10 +12,10 @@ ISSAD::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
