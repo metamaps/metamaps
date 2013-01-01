8820d787cb3b8f31bbea37891cfa53e1a2a177fa
Connor Turland <connor@madhattertech.ca>
2013-01-01 18:07:37 -0500

    more mods to migrate file

--- db/migrate/20130101215010_items_to_topics.rb
+++ db/migrate/20130101215010_items_to_topics.rb
@@ -1,5 +1,8 @@
 class ItemsToTopics < ActiveRecord::Migration
   def change
+    drop_table :topics
+    drop_table :metacodes
+    
     rename_column :items, :item_category_id, :metacode_id
     rename_column :mappings, :item_id, :topic_id
   
