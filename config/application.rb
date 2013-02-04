f402ddc2436f90336ed8163db713690881bcc449
Connor Turland <connor@madhattertech.ca>
2013-02-03 19:52:14 -0500

    changed code for production env so that I can debug

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,6 @@ module ISSAD
     config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '2.0'
   end
 end
