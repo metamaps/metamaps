d283797deba83fa4c95da3ba825ff9b0bb0b2d6f
Devin Howard <devin@callysto.com>
2013-02-22 18:08:03 -0500

    made the metamaps model cooler

--- app/assets/javascripts/Jit/graphsettings-model.js
+++ app/assets/javascripts/Jit/graphsettings-model.js
@@ -6,12 +6,24 @@
  */
 
 var MetamapsModel = new Object();
+
 MetamapsModel.embed = false;
+
+//array of all selected edges
 MetamapsModel.selectedEdges = new Array();
+
+//is any showcard open right now? which one?
 MetamapsModel.showcardInUse = null;
+
+//is the mouse hovering over an edge? which one?
+MetamapsModel.edgeHoveringOver = false;
+
+//double clicking of nodes or canvas
+MetamapsModel.lastNodeClick = 0;
 MetamapsModel.lastCanvasClick = 0;
 MetamapsModel.DOUBLE_CLICK_TOLERANCE = 300;
-MetamapsModel.edgeHoveringOver = false;
+
+//pop-up permission editors timers
 MetamapsModel.edgePermTimer1 = null;
 MetamapsModel.edgePermTimer2 = null;
 MetamapsModel.edgePermSliding = false;
