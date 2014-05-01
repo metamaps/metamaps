e47a3c81e42f88728c07c5d86fad2fbfc2497ea6
Connoropolous <connorturland@gmail.com>
2014-04-30 20:40:48 -0400

    Delete 20140210031525_add_admin_to_users.rb

--- db/migrate/20140210031525_add_admin_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddAdminToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :admin, :boolean
-    User.reset_column_information
-    User.update_all(:admin => false)
-  end
-end
