c79047d8979ffca0f79811107b45d674e19cd5c8
Connoropolous <connorturland@gmail.com>
2014-04-30 20:40:28 -0400

    Delete 20140210025850_add_featured_to_maps.rb

--- db/migrate/20140210025850_add_featured_to_maps.rb
+++
@@ -1,7 +0,0 @@
-class AddFeaturedToMaps < ActiveRecord::Migration
-  def change
-    add_column :maps, :featured, :boolean
-    Map.reset_column_information
-    Map.update_all(:featured => false)
-  end
-end
