39140a06e66cc55425992dfed5340d8294b73116
Connor Turland <connorturland@gmail.com>
2014-08-10 19:52:39 -0400

    moved famous files

--- app/assets/javascripts/metamaps/Metamaps.js
+++ app/assets/javascripts/metamaps/Metamaps.js
@@ -1332,7 +1332,7 @@ Metamaps.Realtime = {
         var mapperm = Metamaps.Active.Map && Metamaps.Active.Map.authorizeToEdit(Metamaps.Active.Mapper);
 
         if (mapperm) {
-            self.socket = io.connect('http://localhost:5001');
+            self.socket = io.connect('http://localhost:5001'); 
             self.socket.on('connect', function () {
                 console.log('socket connected');
                 self.setupSocket();
