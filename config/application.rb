341f9b9bdd3a70f8584a9860ac6ac4deb87b542b
Connor Turland <connorturland@gmail.com>
2014-08-10 19:13:25 -0400

    home page styling

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,8 @@ module Metamaps
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
+      
+    config.assets.paths << "#{Rails.root}/app/assets/javascripts/src"
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '2.0'
