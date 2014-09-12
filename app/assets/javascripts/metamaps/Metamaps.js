b722dc911b0ee92c9cec2142cd0da1a101581e3d
Shai Mor <sj1mor@gmail.com>
2014-09-12 11:45:53 -0400

    help qr tutorials

--- app/assets/javascripts/metamaps/Metamaps.js
+++ app/assets/javascripts/metamaps/Metamaps.js
@@ -3112,8 +3112,26 @@ Metamaps.Map = {
 Metamaps.Map.CheatSheet = {
     init: function () {
         // tab the cheatsheet
-        $('#cheatSheet').tabs().addClass("ui-tabs-vertical ui-helper-clearfix");
-        $("#cheatSheet .ui-tabs-nav li").removeClass("ui-corner-top").addClass("ui-corner-left");
+        $('#cheatSheet').tabs();
+        $('#quickReference').tabs().addClass("ui-tabs-vertical ui-helper-clearfix");
+        $("#quickReference .ui-tabs-nav li").removeClass("ui-corner-top").addClass("ui-corner-left");
+        
+        // id = the id of a vimeo video
+        var switchVideo = function (element, id) {
+            $('.tutorialItem').removeClass("active");
+            $(element).addClass("active");
+            $('#tutorialVideo').attr('src','//player.vimeo.com/video/'+id);
+        };
+
+        $('#gettingStarted').click(function() {
+            switchVideo(this,'88334167');
+        });
+        $('#upYourSkillz').click(function() {
+            switchVideo(this,'100118167');
+        });
+        $('#advancedMapping').click(function() {
+            switchVideo(this,'88334167');
+        });
     }
 }; // end Metamaps.Map.CheatSheet
 
