9a250d0034093bc4006c7ffd8bd4fcb5903d9b51
Connor Turland <connorturland@gmail.com>
2014-10-07 18:38:33 -0400

    got redirect for single sign on with uservoice working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,11 @@ class ApplicationController < ActionController::Base
   
   def after_sign_in_path_for(resource)
     sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
+
     if request.referer == sign_in_url
       super
+    elsif params[:uv_login] == "1"
+      "https://support.metamaps.cc/login_success?sso=" + current_sso_token
     else
       stored_location_for(resource) || request.referer || root_path
     end
