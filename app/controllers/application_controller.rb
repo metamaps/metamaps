a8d173ff1d16111534060c5e0c768d72caa4c5d8
Connor Turland <connorturland@gmail.com>
2014-10-07 18:49:14 -0400

    made http protocol for uservoice redirect consistent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
     if request.referer == sign_in_url
       super
     elsif params[:uv_login] == "1"
-      "https://support.metamaps.cc/login_success?sso=" + current_sso_token
+      "http://support.metamaps.cc/login_success?sso=" + current_sso_token
     else
       stored_location_for(resource) || request.referer || root_path
     end
