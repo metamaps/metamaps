7e182b8bebb44ee35febdac6e09a6f3338f644de
Connor Turland <connorturland@gmail.com>
2014-10-08 03:24:08 -0400

    figured out phantom?

--- app/workers/grab_map_screenshot_worker.rb
+++ app/workers/grab_map_screenshot_worker.rb
@@ -1,14 +1,9 @@
-require 'phantomjs'
-
 # app/workers/grab_map_screenshot_worker.rb
 class GrabMapScreenshotWorker
   include Sidekiq::Worker
 
   def perform(map_id)
-    imgBase64 = ''
-    Phantomjs.run('./script/phantomjs-save-screenshot.js', map_id.to_s) { |line|
-      imgBase64 << line
-    }
+    imgBase64 = `phantomjs ./script/phantomjs-save-screenshot.js #{map_id}`
 	map = Map.find(map_id)
     map.decode_base64(imgBase64)
   end
