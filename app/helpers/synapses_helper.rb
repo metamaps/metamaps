4831307fe7748fff8c75b640204d8bb9ccfe6b9c
Connor Turland <connorturland@gmail.com>
2014-11-16 15:22:29 -0500

    fixed user image proportions in synapse autocomplete

--- app/helpers/synapses_helper.rb
+++ app/helpers/synapses_helper.rb
@@ -25,7 +25,7 @@ module SynapsesHelper
       synapse['permission'] = s.permission
       synapse['mapCount'] = s.maps.count
       synapse['originator'] = s.user.name
-      synapse['originatorImage'] = s.user.image
+      synapse['originatorImage'] = s.user.image.url(:square)
       synapse['rtype'] = "synapse"
       
       temp.push synapse
