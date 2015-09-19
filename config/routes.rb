1fbd16ac95bde1cdd3817771779b34a5bbe9aed2
Devin Howard <devin@callysto.com>
2015-09-19 20:46:10 +0800

    fix problem with join route

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Metamaps::Application.routes.draw do
     get 'login' => 'devise/sessions#new', :as => :new_user_session
     post 'login' => 'devise/sessions#create', :as => :user_session
     get 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
-#    get 'join' => 'devise/registrations#new', :as => :new_user_registration
+    get 'join' => 'devise/registrations#new', :as => :new_user_registration_path
   end
 
   get 'users/:id/details', to: 'users#details', as: :details
