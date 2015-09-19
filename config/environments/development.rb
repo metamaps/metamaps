f5dbfd5d720a3cfc7d560ea1a338e5eb974bf7b8
Connor Turland <connorturland@gmail.com>
2015-09-19 13:25:30 -0400

    configuration for production environments like heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Metamaps::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.log_level = :info
   config.eager_load = false
 
   # In the development environment your application's code is reloaded on
