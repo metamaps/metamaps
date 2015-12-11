5f1ef5f5c8d7b87ffd4db378b0344e611c8ede29
Connor Turland <connorturland@gmail.com>
2015-12-11 18:10:38 -0500

    remove extra code

--- realtime/signal.js
+++ realtime/signal.js
@@ -3,20 +3,15 @@ var uuid = require('node-uuid');
 module.exports = function(io, stunservers) {
 
     var
-      activePeople = 0,
-      activeRooms = {};
+      activePeople = 0;
 
     function describeRoom(name) {
         var clients = io.sockets.clients(name);
         var result = {
-            clients: {},
-            avatars: {},
-            usernames: {}
+            clients: {}
         };
         clients.forEach(function (client) {
             result.clients[client.id] = client.resources;
-            result.avatars[client.id] = client.avatar;
-            result.usernames[client.id] = client.username;
         });
         return result;
     }
@@ -31,14 +26,6 @@ module.exports = function(io, stunservers) {
 
     io.sockets.on('connection', function (client) {
         activePeople += 1;
-
-        client.on('setDetails', function (details) {
-            client.profile = {
-                username: details.username,
-                image: details.image
-            };
-            client.broadcast.emit('presence', client.profile);
-        });
 
         client.resources = {
             screen: false,
@@ -72,26 +59,12 @@ module.exports = function(io, stunservers) {
         function videoAdded(data) {
           var socketsInRoom = io.sockets.clients(client.room);
           client.resources.video = true;
-          client.avatar = data.avatar;
-          client.username = data.username;
           socketsInRoom.forEach(function(socket) {
             if (socket.id !== client.id) {
-              socket.emit('addVideo', { id: client.id, avatar: data.avatar, username: data.username });
+              socket.emit('addVideo', { id: client.id });
             }
           });
         }
-
-        client.on('requestRoomCount', function(name) {
-            client.emit('room_count', {
-                room_id: name,
-                count: io.sockets.clients(name).length
-            });
-        });
-
-        client.on('requestActiveRooms', function() {
-            client.emit('rooms_count', Object.keys(activeRooms).length);
-        });
-
 
         function removeFeed(type) {
             if (client.room) {
@@ -101,22 +74,6 @@ module.exports = function(io, stunservers) {
                 });
                 if (!type) {
                     client.leave(client.room);
-                }
-                io.sockets.emit('room_count', {
-                    room_id: client.room,
-                    count: io.sockets.clients(client.room).length
-                });
-                if (client.profile) {
-                    client.broadcast.emit('vacated_room', {
-                        room_id: client.room,
-                        profile: client.profile
-                    });
-                }
-                if (io.sockets.clients(client.room).length == 0) {
-                    delete activeRooms[client.room];
-                    io.sockets.emit('rooms_count', Object.keys(activeRooms).length);
-                }
-                if (!type) {
                     client.room = undefined;
                 }
             }
@@ -129,27 +86,7 @@ module.exports = function(io, stunservers) {
             removeFeed();
             safeCb(cb)(null, describeRoom(name));
             client.join(name);
-            io.sockets.emit('room_count', {
-                room_id: name,
-                count: io.sockets.clients(name).length
-            });
-            if (client.profile) {
-                client.broadcast.emit('presence_room', {
-                    room_id: name,
-                    profile: client.profile
-                });
-            }
-            io.sockets.clients(name).forEach(function (s) {
-                if (s.profile) {
-                    client.emit('presence_room', {
-                        room_id: name,
-                        profile: s.profile
-                    });
-                }
-            });
             client.room = name;
-            activeRooms[name] = true;
-            io.sockets.emit('rooms_count', Object.keys(activeRooms).length);
         }
 
         // we don't want to pass "leave" directly because the
@@ -157,10 +94,6 @@ module.exports = function(io, stunservers) {
         client.on('disconnect', function () {
             removeFeed();
             activePeople -= 1;
-            io.sockets.emit('users_count', activePeople);
-            if (client.profile) {
-                io.sockets.emit('vacated', client.profile);
-            }
         });
         client.on('leave', function () {
             removeFeed();
@@ -185,11 +118,5 @@ module.exports = function(io, stunservers) {
 
         // tell client about stun and turn servers and generate nonces
         client.emit('stunservers', stunservers || []);
-
-        client.emit('rooms_count', Object.keys(activeRooms).length);
-        io.sockets.emit('users_count', activePeople);
-        io.sockets.clients().forEach(function (socket) {
-            if (socket.id !== client.id && socket.profile) client.emit('presence', socket.profile);
-        });
     });
 };
