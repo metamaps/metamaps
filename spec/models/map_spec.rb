63e493959213426773e2191fef51d35a179c48e9
Devin Howard <devin@callysto.com>
2015-12-18 09:35:40 +0800

    remove TODO

--- spec/models/map_spec.rb
+++ spec/models/map_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Map, type: :model do
-  # TODO: what is it important to be sure about when working with a Map?
   it { is_expected.to belong_to :user }
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_presence_of :permission }
