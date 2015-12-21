914ff46bb9c37478939733f05dcfc010eb6b018b
Devin Howard <devin@callysto.com>
2015-12-21 11:40:30 +0800

    syntax error in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Metamaps::Application.configure do
   # S3 file storage
   config.paperclip_defaults = {
     storage: :s3,
-    :s3_credentials: {
+    s3_credentials: {
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
