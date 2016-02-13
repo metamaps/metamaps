52facb9c1da665e36ea6f81b9a712fa6e1f1a361
Devin Howard <devin@callysto.com>
2016-02-13 17:28:16 +0800

    topic policy

---
+++ app/policies/topic_policy.rb
@@ -0,0 +1,40 @@
+class TopicPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where('permission IN ("public", "commons") OR user_id = ?', user.id)
+    end
+  end
+
+  def create?
+    user.present?
+  end
+
+  def show?
+    record.permission == 'commons' || record.permission == 'public' || record.user == user
+  end
+
+  def update?
+    # user.present? && (record.permission == 'commons' || record.user == user)
+    true
+  end
+
+  def destroy?
+    record.user == user || user.admin
+  end
+
+  def autocomplete_topic?
+    user.present?
+  end
+
+  def network?
+    show?
+  end
+
+  def relative_numbers?
+    show?
+  end
+
+  def relatives?
+    show?
+  end
+end
