ccfba03fdb1ed5a858f3573f84026ee90c0b7c77
Connor Turland <connorturland@gmail.com>
2016-03-11 17:26:54 +1100

    clean up duplicate code

--- app/controllers/api/maps_controller.rb
+++ app/controllers/api/maps_controller.rb
@@ -1,11 +1,3 @@
 class Api::MapsController < API::RestfulController
 
-  def create
-    raise CanCan::AccessDenied.new unless current_user.is_logged_in?
-    instantiate_resouce
-    resource.user = current_user
-    create_action
-    respond_with_resource
-  end
-
 end
--- app/controllers/api/restful_controller.rb
+++ app/controllers/api/restful_controller.rb
@@ -1,6 +1,14 @@
 class API::RestfulController < ActionController::Base
   snorlax_used_rest!
 
+  def create
+    raise CanCan::AccessDenied.new unless current_user.is_logged_in?
+    instantiate_resouce
+    resource.user = current_user
+    create_action
+    respond_with_resource
+  end
+
   def show
     load_resource
     raise AccessDenied.new unless resource.authorize_to_show(current_user)
