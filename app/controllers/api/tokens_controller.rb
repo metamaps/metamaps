521aa6b5d00cda63d6b64210cecc861bc52197a4
Connor Turland <connorturland@gmail.com>
2016-03-12 11:58:26 +1100

    function no longer exists

--- app/controllers/api/tokens_controller.rb
+++ app/controllers/api/tokens_controller.rb
@@ -3,7 +3,7 @@ class Api::TokensController < API::RestfulController
   skip_authorization
  
   def my_tokens
-    raise Pundit::NotAuthorizedError.new unless current_user.is_logged_in?
+    raise Pundit::NotAuthorizedError.new unless current_user
     instantiate_collection page_collection: false, timeframe_collection: false
     respond_with_collection
   end
