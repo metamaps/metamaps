6f5258cbb7bd5ed145dba95d2d1c594f41fbbe85
Connor Turland <connorturland@gmail.com>
2016-03-14 08:19:26 +1100

    needed a policy for tokens

--- app/serializers/token_serializer.rb
+++ app/serializers/token_serializer.rb
@@ -1,9 +1,7 @@
 class TokenSerializer < ActiveModel::Serializer
-  embed :ids, include: true
   attributes :id,
              :token,
              :description,
-             :user_id,
              :created_at,
              :updated_at
 
