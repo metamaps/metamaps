e27d64e64384a432297f2446683b782eb4e860d1
Devin Howard <devin@callysto.com>
2016-04-06 20:09:25 +0800

    only allow cors on api routes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*',
+    resource '/api/*',
       headers: :any,
       methods: [:get, :post, :put, :delete, :options, :head]
   end
