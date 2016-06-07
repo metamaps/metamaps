1d12aed3ebf206f796a083c3e076f6acc35e930a
Devin Howard <devin@callysto.com>
2016-06-07 12:55:49 +0800

    switch to dotenv-rails

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'csv'
 require 'rails/all'
-require 'dotenv'
 
 Bundler.require(*Rails.groups)
-
-ENV["RAILS_ENV"] ||= ENV["RACK_ENV"] || "development"
-Dotenv.load ".env.#{ENV["RAILS_ENV"]}", '.env'
 
 module Metamaps
   class Application < Rails::Application
