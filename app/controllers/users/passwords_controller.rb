4723c62b2025ffdb833bb918760bd05af9c74245
Devin Howard <devin@callysto.com>
2016-09-15 07:18:15 +0800

    fix password reset error

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,7 +1,11 @@
 class Users::PasswordsController < Devise::PasswordsController
   protected
 
-    def after_resetting_password_path_for(resource)
-      signed_in_root_path(resource)
-    end
+  def after_resetting_password_path_for(resource)
+    signed_in_root_path(resource)
+  end
+
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    new_user_session_path if is_navigational_format?
+  end
 end
