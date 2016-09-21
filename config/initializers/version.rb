8b19c9e340482d16b703d1a089baa9c5b434e5bb
Devin Howard <devin@callysto.com>
2016-09-22 01:24:14 +0800

    automatic versioning via git (#621)

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,2 +1,2 @@
-METAMAPS_VERSION = '2.9.0'.freeze
-METAMAPS_LAST_UPDATED = 'Sept 1, 2016'.freeze
+METAMAPS_VERSION = "2 build `git log -1 --pretty=%H`".freeze
+METAMAPS_LAST_UPDATED = `git log -1 --pretty='%ad' --date=format:'%b %d, %Y'`.freeze
