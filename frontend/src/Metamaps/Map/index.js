f41ece6f1c5a7e59c560518a1b57d81a31df0398
Robert Best <chessscholar@gmail.com>
2016-09-23 15:47:37 -0400

    Update index.js

--- frontend/src/Metamaps/Map/index.js
+++ frontend/src/Metamaps/Map/index.js
@@ -76,6 +76,8 @@ const Map = {
       var map = Active.Map
       var mapper = Active.Mapper
 
+      document.title = map.attributes.name + ' | Metamaps'
+
       // add class to .wrapper for specifying whether you can edit the map
       if (map.authorizeToEdit(mapper)) {
         $('.wrapper').addClass('canEditMap')
