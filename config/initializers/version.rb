a7338f8960d0b9fe7296d38d196cff007b5e263e
Devin Howard <devin@callysto.com>
2016-09-23 17:49:26 +0800

    safer git dating

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,2 +1,2 @@
 METAMAPS_VERSION = "2 build `git log -1 --pretty=%H`".freeze
-METAMAPS_LAST_UPDATED = `git log -1 --pretty='%ad' --date=format:'%b %d, %Y'`.freeze
+METAMAPS_LAST_UPDATED = `git log -1 --pretty='%ad'`.split(' ').values_at(1,2,4).join(' ').freeze
