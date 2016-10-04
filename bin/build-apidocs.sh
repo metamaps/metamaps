15b8440fbcaf4d51f8b98011b01fe8dcb030d9e3
Devin Howard <devin@callysto.com>
2016-10-04 16:21:09 +0800

    move raml2html to optional dependencies so it can be installed globally

--- bin/build-apidocs.sh
+++ bin/build-apidocs.sh
@@ -2,4 +2,8 @@
 
 # Note: you need to run `npm install` before using this script or raml2html won't be installed
 
+if [[ ! -x ./node_modules/.bin/raml2html ]]; then
+  npm install
+fi
+
 ./node_modules/.bin/raml2html -i ./doc/api/api.raml -o ./public/api/index.html
