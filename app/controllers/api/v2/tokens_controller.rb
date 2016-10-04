8afef1bc4a66357ec11daa6af5dab94125c40089
Devin Howard <devin@callysto.com>
2016-10-04 16:02:58 +0800

    make tokens description field optional

--- app/controllers/api/v2/tokens_controller.rb
+++ app/controllers/api/v2/tokens_controller.rb
@@ -6,6 +6,19 @@ module Api
         [:description]
       end
 
+      def create
+        if params[:token].blank?
+          self.resource = resource_class.new
+        else
+          instantiate_resource
+        end
+
+        resource.user = current_user if current_user.present?
+        authorize resource
+        create_action
+        respond_with_resource
+      end
+
       def my_tokens
         authorize resource_class
         instantiate_collection
