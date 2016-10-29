b9190233b199edeacd599899323070f485ecd9ac
Devin Howard <devin@callysto.com>
2016-10-29 23:38:17 +0800

    enable sucker punch for heroku (#878)

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,10 @@ Bundler.require(*Rails.groups)
 module Metamaps
   class Application < Rails::Application
     config.active_job.queue_adapter = :delayed_job
+    if ENV['ACTIVE_JOB_FRAMEWORK'] == 'sucker_punch'
+      config.active_job.queue_adapter = :sucker_punch
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
