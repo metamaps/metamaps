62e2acdd526f1c07debc6150a6c62ac606fcc08f
Devin Howard <devin@callysto.com>
2016-10-31 15:38:09 +0800

    add about lightbox again (#884)
    * add about lightbox again
    
    * fix about lightbox style

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 METAMAPS_VERSION = '2.9'
-METAMAPS_BUILD = `git log -1 --pretty=%H`.chomp.freeze
+METAMAPS_BUILD = `git log -1 --pretty=%H`.chomp[0..11].freeze
 METAMAPS_LAST_UPDATED = `git log -1 --pretty='%ad'`.split(' ').values_at(1, 2, 4).join(' ').freeze
