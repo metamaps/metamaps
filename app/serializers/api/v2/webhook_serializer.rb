a9c67e70d4d678cdc4854fbe71ee518d12082fe2
Devin Howard <devin@callysto.com>
2016-11-01 09:46:44 +0800

    move webhook and event serializers back (#899)

--- app/serializers/api/v2/webhook_serializer.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-module Api
-  module V2
-    class WebhookSerializer < ApplicationSerializer
-      attributes :text, :username, :icon_url # , :attachments
-    end
-  end
-end
