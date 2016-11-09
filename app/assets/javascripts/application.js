4533a0f2fee3ac51464a5fa8cf89ae2435158527
Devin Howard <devin@callysto.com>
2016-11-09 09:34:10 -0500

    merge changes from metamapscc instance branch (#913)
    * homepage video fallback
    
    * public/50x.html
    
    * remove blog from public/ html pages
    
    * remove video file since it's on metamaps.cc server
    
    * eslint

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+// eslint-disable spaced-comment
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -16,3 +17,4 @@
 //= require_directory ./lib
 //= require ./webpacked/metamaps.bundle
 //= require ./Metamaps.ServerData
+//= require homepageVimeoFallback
