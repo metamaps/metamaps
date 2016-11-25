8267054d898602a244bb8da8b98174101af4fc3f
Devin Howard <devin@callysto.com>
2016-11-25 15:57:12 -0500

    fugly integration of map mailers into mailboxer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,4 +2,8 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'team@metamaps.cc'
   layout 'mailer'
+
+  def deliver
+    fail NotImplementedError('Please use Mailboxer to send your emails.')
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -8,14 +8,15 @@ Bundler.require(*Rails.groups)
 
 module Metamaps
   class Application < Rails::Application
-    config.active_job.queue_adapter = :delayed_job
-    if ENV['ACTIVE_JOB_FRAMEWORK'] == 'sucker_punch'
-      config.active_job.queue_adapter = :sucker_punch
-    end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #
+    if ENV['ACTIVE_JOB_FRAMEWORK'] == 'sucker_punch'
+      config.active_job.queue_adapter = :sucker_punch
+    else
+      config.active_job.queue_adapter = :delayed_job
+    end
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths << Rails.root.join('app', 'services')
