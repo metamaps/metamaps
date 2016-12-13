87228c27c1a286789a5ef8f8a191fa6346307439
Connor Turland <connorturland@gmail.com>
2016-12-13 02:42:33 -0500

    Fix mailboxer + email bugs

--- app/mailers/map_mailer.rb
+++ app/mailers/map_mailer.rb
@@ -2,17 +2,21 @@
 class MapMailer < ApplicationMailer
   default from: 'team@metamaps.cc'
 
-  def access_request_email(request, map)
+  def access_request_email(request)
     @request = request
-    @map = map
-    subject = @map.name + ' - request to edit'
-    mail(to: @map.user.email, subject: subject)
+    @map = request.map
+    mail(to: @map.user.email, subject: request.requested_text)
+  end
+
+  def access_approved_email(request)
+    @request = request
+    @map = request.map
+    mail(to: request.user, subject: request.approved_text)
   end
 
   def invite_to_edit_email(map, inviter, invitee)
     @inviter = inviter
     @map = map
-    subject = @map.name + ' - invitation to edit'
-    mail(to: invitee.email, subject: subject)
+    mail(to: invitee.email, subject: map.invited_text)
   end
 end
---
+++ app/services/notification_service.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+class NotificationService
+
+  def self.renderer
+    renderer ||= ApplicationController.renderer.new(
+      http_host: ENV['MAILER_DEFAULT_URL'],
+      https:     Rails.env.production? ? true : false
+    )
+  end
+
+  def self.access_request(request)
+    body = renderer.render(template: 'map_mailer/access_request_email', locals: { map: request.map, request: request }, layout: false)
+    request.map.user.notify(request.requested_text, body, request, false, MAILBOXER_CODE_ACCESS_REQUEST)
+  end
+
+  def self.access_approved(request)
+    body = renderer.render(template: 'map_mailer/access_approved_email', locals: { map: request.map }, layout: false)
+    receipt = request.user.notify(request.approved_text, body, request, false, MAILBOXER_CODE_ACCESS_APPROVED)
+  end
+
+  def self.invite_to_edit(map, inviter, invited)
+    user_map = UserMap.find_by(user: invited, map: map)
+    body = renderer.render(template: 'map_mailer/invite_to_edit_email', locals: { map: map, inviter: inviter }, layout: false)
+    invited.notify(map.invited_text, body, user_map, false, MAILBOXER_CODE_INVITE_TO_EDIT)
+  end
+end
