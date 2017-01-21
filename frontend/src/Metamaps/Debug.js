ba3d5f07ddf6b357471dc67d5d816ec4bb3e38d1
Devin Howard <devin@callysto.com>
2017-01-21 13:09:26 -0500

    try to fix metamaps.debug

--- frontend/src/Metamaps/Debug.js
+++ frontend/src/Metamaps/Debug.js
@@ -1,6 +1,9 @@
-const Debug = (arg = window.Metamaps) => {
+const Debug = function(arg = window.Metamaps) => {
+  if (arg === undefined && typeof window !== 'undefined') arg = window.Metamaps
   console.debug(arg)
-  console.debug(`Metamaps Version: ${arg.VERSION}`)
+  console.debug(`Metamaps Version: ${arg.ServerData.VERSION}`)
+  console.debug(`Build: ${arg.ServerData.BUILD}`)
+  console.debug(`Last Updated: ${arg.ServerData.LAST_UPDATED}`)
 }
 
 export default Debug
