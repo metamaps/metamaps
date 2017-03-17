c8f6374ac821e12b2dca34d738c2fc86f4db8005
Connor Turland <connorturland@gmail.com>
2017-03-17 09:32:52 -0400

    remove no longer needed comments

--- frontend/src/Metamaps/Map/CheatSheet.js
+++ frontend/src/Metamaps/Map/CheatSheet.js
@@ -6,23 +6,6 @@ const CheatSheet = {
     $('#cheatSheet').tabs()
     $('#quickReference').tabs().addClass('ui-tabs-vertical ui-helper-clearfix')
     $('#quickReference .ui-tabs-nav li').removeClass('ui-corner-top').addClass('ui-corner-left')
-
-    // // id = the id of a vimeo video
-    // var switchVideo = function(element, id) {
-    //   $('.tutorialItem').removeClass('active')
-    //   $(element).addClass('active')
-    //   $('#tutorialVideo').attr('src', '//player.vimeo.com/video/' + id)
-    // }
-
-    // $('#gettingStarted').click(function() {
-    //   switchVideo(this,'88334167')
-    // })
-    // $('#upYourSkillz').click(function() {
-    //   switchVideo(this,'100118167')
-    // })
-    // $('#advancedMapping').click(function() {
-    //   switchVideo(this,'88334167')
-    // })
   }
 }
 
