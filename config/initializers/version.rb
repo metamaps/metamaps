328c1a14f3efca9f68c3514798c486cd8a172374
Devin Howard <devin@callysto.com>
2017-05-13 19:32:51 -0700

    mark v3.4.1 one commit too late

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
-METAMAPS_VERSION = '3.4'
+METAMAPS_VERSION = '3.4.1'
 METAMAPS_BUILD = `git log -1 --pretty=%H`.chomp[0..11].freeze
 METAMAPS_LAST_UPDATED = `git log -1 --pretty='%ad'`.split(' ').values_at(1, 2, 4).join(' ').freeze
